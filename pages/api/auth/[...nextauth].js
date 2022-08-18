fcc76679d69f8b60caf9b503f9e4907ffc588af0
Phillip Friedel <s0557917@htw-berlin.de>
2022-08-18 21:49:46 +0200

    Added nextauth google provider

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,7 +1,8 @@
-import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
-import { PrismaAdapter } from "@next-auth/prisma-adapter"
-import { PrismaClient } from "@prisma/client"
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient()
 
@@ -9,9 +10,30 @@ export default NextAuth({
     adapter: PrismaAdapter(prisma),
     providers: [
         GithubProvider({
-        clientId: process.env.GITHUB_ID,
-        clientSecret: process.env.GITHUB_SECRET,
+            clientId: process.env.GITHUB_ID,
+            clientSecret: process.env.GITHUB_SECRET,
+        }),
+        GoogleProvider({
+            clientId: process.env.GOOGLE_ID,
+            clientSecret: process.env.GOOGLE_SECRET,
         }),
     ],
+    jwt: {
+        encryption: true,
+    },
     secret: process.env.SECRET,
+    callbacks: {
+        async jwt(token, account) {
+            if (account?.accessToken) {
+                token.accessToken = account.accessToken;
+            }
+            return token;
+        },
+        redirect: async (url, baseUrl) => {
+            if(url === '/task-list' || url === '/calendar' || url === '/text-editor') {
+                return Promise.resolve('/');
+            }
+            return Promise.resolve('/');
+        }
+    }
 })
