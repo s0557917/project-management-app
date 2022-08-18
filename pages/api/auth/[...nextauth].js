d6cec64f03dec692ce3d395692977c8a764a2efa
Phillip Friedel <s0557917@htw-berlin.de>
2022-08-18 16:48:19 +0200

    Added missing nextauth secret

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -12,6 +12,6 @@ export default NextAuth({
         clientId: process.env.GITHUB_ID,
         clientSecret: process.env.GITHUB_SECRET,
         }),
-        // ...add more providers here
     ],
+    secret: process.env.SECRET,
 })
